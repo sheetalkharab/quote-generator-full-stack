cf8109d80cae3311b9d0ff9dc078b91f0b1a5922
sheetalkharab <sheetalrana89@gmail.com>
2025-08-29 19:29:32 +0100

    little update in port code

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cors());
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 function pickRandomQuote() {
   const index = Math.floor(Math.random() * quotes.length);
