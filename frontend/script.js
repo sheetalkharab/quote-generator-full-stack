a5e46604d896010790791f1269a645f02f277b2a
sheetalkharab <sheetalrana89@gmail.com>
2025-09-01 12:56:38 +0100

    in script.js file backend url updated

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,7 @@
 async function fetchQuote() {
-  const res = await fetch("http://127.0.0.1:3000/");
+  const res = await fetch(
+    "https://v0c4so84scsckggcgwsk0so4.hosting.codeyourfuture.io/"
+  );
   const data = await res.json();
   document.getElementById("quote").innerText = `"${data.quote}"`;
   document.getElementById("author").innerText = `- ${data.author}`;
