7bde7df94c41b57327e95c56afe8f210f77eb4cb
sheetalkharab <sheetalrana89@gmail.com>
2025-09-07 22:22:40 +0100

    add post method

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import { quotes } from "./quotes.js";
-import express from "express";
+import express, { text } from "express";
 import cors from "cors";
 
 const app = express();
@@ -16,6 +16,25 @@ app.get("/", (req, res) => {
   console.error("Received a request for a quote");
   const quote = pickRandomQuote();
   res.json(quote);
+});
+
+app.post("/", (req, res) => {
+  const newQuote = {
+    quote: req.body.quote,
+    author: req.body.author,
+  };
+
+  if (!newQuote.quote || !newQuote.author) {
+    return res
+      .status(400)
+      .json({ error: "Both Quote and author are required" });
+  }
+  quotes.push(newQuote);
+
+  res.status(201).json({
+    message: "New quote added successfully!",
+    added: newQuote,
+  });
 });
 
 app.listen(port, () => {
